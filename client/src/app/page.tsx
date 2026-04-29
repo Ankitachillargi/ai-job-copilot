e47e4a25d607ce1e0a0f1026b638f5371115ba1a
Ankitachillargi <147800625+Ankitachillargi@users.noreply.github.com>
2026-04-29 18:03:46 +0530

    Minor Changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function DashboardPage() {
 
   return (
     <div className="flex min-h-screen bg-zinc-950 text-white">
-      {/* Sidebar */}
+      {/* Sidebar Contet will go here*/}
       <aside className="w-64 bg-zinc-900 p-5 hidden md:flex flex-col justify-between">
         <div>
           <h2 className="text-xl font-bold mb-6">AI Copilot</h2>
