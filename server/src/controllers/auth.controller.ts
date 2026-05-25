685a11f2524b6bc7c250148c61b22bc5af3657b4
Ankitachillargi <147800625+Ankitachillargi@users.noreply.github.com>
2026-05-25 15:09:19 +0530

    Login Duplicate check!

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -24,6 +24,7 @@ export const registerUser = async (req: Request, res: Response) => {
   }
 };
 
+//Login Functionality here..
 export const loginUser = async (req: Request, res: Response) => {
   try {
 
