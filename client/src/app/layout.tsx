9113371ad7a33d7e83592b652aeaaace61dc2af0
Ankitachillargi <147800625+Ankitachillargi@users.noreply.github.com>
2026-06-17 18:33:56 +0530

    Updated layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const loadFromStorage = useAuthStore((s) => s.loadFromStorage);
-
+//UseEffect
   useEffect(() => {
     console.log("🔥 Running loadFromStorage");
     loadFromStorage();
